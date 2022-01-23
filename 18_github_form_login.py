076c40a03a3c50434bbe8479addafeabb61facd3
alonsomedo <alonso2295@hotmail.com>
2022-01-23 12:08:08 -0500

    github through form login

---
+++ 18_github_form_login.py
@@ -0,0 +1,48 @@
+from email import header
+from ssl import SSLSession
+from urllib import request
+import requests
+from lxml import html
+import json
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+}
+
+
+login_url = 'https://github.com/login'
+
+session = requests.Session()
+
+login_response = requests.get(login_url, headers = headers)
+
+parser = html.fromstring(login_response.text)
+
+token = parser.xpath('//input[@name="authenticity_token"]/@value')
+
+session_url = 'https://github.com/session'
+
+login_data = {
+  "login": open('./credentials.txt').readline().split(',')[0],
+  "password": open('./credentials.txt').readline().split(',')[1],
+  "commit": "Sign in", 
+  "authenticity_token":  token
+}
+
+session.post(
+    session_url,
+    data = login_data,
+    headers = headers
+)
+
+data_url = 'https://github.com/alonsomedo?tab=repositories'
+response_data = session.get(data_url, headers = headers)
+data_parser = html.fromstring(response_data.text)
+
+repositories = data_parser.xpath('//h3[@class="wb-break-all"]/a/text()')
+
+for r in repositories:
+    print(r)
+
+
+
